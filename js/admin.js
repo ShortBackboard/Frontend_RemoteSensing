fc87e640b2b744c93eb6ed830a982555ec8f0a71
lichengyang <lichengyang1024@126.com>
2025-02-11 11:32:00 +0800

    分页显示数据前端实现

--- js/admin.js
+++ js/admin.js
@@ -4,35 +4,49 @@ const app = new Vue ({
             msg: '多平台遥感协同叶绿素浓度监测系统',
             users : [
                 {
-                    id: 95001,
-                    name: '小a',
-                    gender: '男',
-                    birthday: '2000-01-01',
-                    tel: '12345678901',
-                    email: '12345678901@qq.com',
-                    address: '北京市',
-                    career: '园林遥感'
+                    id: 95001,name: '小a',gender: '男',birthday: '2000-01-01',tel: '12345678901',
+                    email: '12345678901@qq.com',address: '北京市',career: '园林遥感'
                 },
                 {
-                    id: 95002,
-                    name: '小b',
-                    gender: '女',
-                    birthday: '2000-02-02',
-                    tel: '12345678902',
-                    email: '12345678902@qq.com',
-                    address: '上海市',
-                    career: '水色遥感'
+                    id: 95002,name: '小b',gender: '女',birthday: '2000-01-01',tel: '12345678901',
+                    email: '12345678901@qq.com',address: '北京市',career: '园林遥感'
                 },
                 {
-                    id: 95003,
-                    name: '小c',
-                    gender: '男',
-                    birthday: '2000-03-03',
-                    tel: '12345678903',
-                    email: '12345678903@qq.com',
-                    address: '广州市',
-                    career: '植被遥感'
+                    id: 95003,name: '小c',gender: '男',birthday: '2000-01-01',tel: '12345678901',
+                    email: '12345678901@qq.com',address: '北京市',career: '园林遥感'
+                },
+                {   
+                    id: 95004,name: '小d',gender: '女',birthday: '2000-01-01',tel: '12345678901',
+                    email: '12345678901@qq.com',address: '北京市',career: '园林遥感'
+                },
+                {
+                    id: 95005,name: '小e',gender: '男',birthday: '2000-01-01',tel: '12345678901',
+                    email: '12345678901@qq.com',address: '北京市',career: '园林遥感'
+                },
+                {
+                    id: 95001,name: '小a',gender: '男',birthday: '2000-01-01',tel: '12345678901',
+                    email: '12345678901@qq.com',address: '北京市',career: '园林遥感'
+                },
+                {
+                    id: 95002,name: '小b',gender: '女',birthday: '2000-01-01',tel: '12345678901',
+                    email: '12345678901@qq.com',address: '北京市',career: '园林遥感'
+                },
+                {
+                    id: 95003,name: '小c',gender: '男',birthday: '2000-01-01',tel: '12345678901',
+                    email: '12345678901@qq.com',address: '北京市',career: '园林遥感'
+                },
+                {   
+                    id: 95004,name: '小d',gender: '女',birthday: '2000-01-01',tel: '12345678901',
+                    email: '12345678901@qq.com',address: '北京市',career: '园林遥感'
+                },
+                {
+                    id: 95005,name: '小e',gender: '男',birthday: '2000-01-01',tel: '12345678901',
+                    email: '12345678901@qq.com',address: '北京市',career: '园林遥感'
                 }
-            ]
+            ],
+
+            total:100, // 数据总数
+            currentpage:1, // 当前页码
+            pagesize:10, // 每页显示条数
         }
 })
