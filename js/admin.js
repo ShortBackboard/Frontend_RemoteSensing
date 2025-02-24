635de1bde7960e96f35f1309cc81cc6f3b2c55f9
lichengyang <lichengyang1024@126.com>
2025-02-24 21:30:45 +0800

    校验账号是否存在

--- js/admin.js
+++ js/admin.js
@@ -1,70 +1,106 @@
-const app = new Vue ({
-    el:'#app',
-    data: {
-        users:[], // 所有用户信息
+const app = new Vue({
+    el: '#app',
+    data() {
+        // 校验账号是否已经存在
+        const rulesNo = (rule, value, callback) => {
+            if (this.isEdit) {
+                callback();
+            }
+            //使用Axios进行校验 
+            axios.post(
+                this.baseURL + 'no/check/',
+                {
+                    no: value,
+                }
+            )
+                .then((res) => {
+                    //请求成功
+                    if (res.data.code === 1) {
+                        if (res.data.exists) {
+                            callback(new Error("账号已存在！"));
+                        } else {
+                            callback();
+                        }
+                    } else {
+                        //请求失败
+                        callback(new Error("校验账号后端出现异常！"))
+                    }
+                })
+                .catch((err) => {
+                    //如果请求失败在控制台打印
+                    console.log(err);
+                });
+        }
 
-        baseURL: "http://127.0.0.1:8000/",
-        inputStr: "", // 搜索框输入内容
-        
-        msg: "多平台遥感协同叶绿素浓度监测系统",
+        return {
+            
+            users: [], // 所有用户信息
 
-        // 分页相关的变量
-        total:0, // 数据总数, 从后端获取
-        currentpage:1, // 当前页码
-        pagesize:10, // 每页显示条数
-        pageUsers:[], // 当前页用户信息
+            baseURL: "http://127.0.0.1:8000/",
+            inputStr: "", // 搜索框输入内容
 
-        // 模态框相关变量
-        dialogTitle: "", // 模态框标题
-        dialogVisible: false, // 控制用户信息对话框的显示
-        isView: false, // 是否为查看模式
-        isEdit: false, // 是否为修改模式
+            msg: "多平台遥感协同叶绿素浓度监测系统",
 
-        userInfo: { // 用户信息
-            no: "",
-            name: "",
-            gender: "",
-            birthday: "",
-            mobile: "",
-            email: "",
-            address: "",
-            career: "",
-            image: "",  // 头像
-        },
+            // 分页相关的变量
+            total: 0, // 数据总数, 从后端获取
+            currentpage: 1, // 当前页码
+            pagesize: 10, // 每页显示条数
+            pageUsers: [], // 当前页用户信息
 
-        //添加用户的表单信息提交前的校验
-        rules: {
-            no: [
-                { required: true, message: '账号不能为空', trigger: 'blur' },
-                { pattern: /\d{5}$/, message: '账号必须是五位数', trigger: 'blur' },
-            ],
-            name: [
-                { required: true, message: '姓名不能为空', trigger: 'blur' },
-                { pattern: /^[\u4e00-\u9fa5]{2,5}$/, message: '姓名必须是2-5个汉字', trigger: 'blur' },
-            ],
-            gender: [
-                { required: true, message: '性别不能为空', trigger: 'change' },
-            ],
-            birthday: [
-                { type:'date', required: true, message: '出生日期不能为空', trigger: 'change' },
-            ],
-            mobile: [
-                { required: true, message: '手机号码不能为空', triggler: 'blur' },
-                { pattern: /^[1][35789]\d{9}$/, message: '手机号码必须要符合规范', trigger: 'blur' },
-            ],
-            email: [
-                { required: true, message: '邮箱地址不能为空', trigger: 'blur' },
-                { pattern: /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/, message: '邮箱地址必须要符合规范', trigger: 'blur' },
-            ],
-            address: [
-                { required: true, message: '家庭住址不能为空', trigger: 'blur' },
-            ],
-            career: [
-                { required: true, message: '研究领域不能为空', trigger: 'blur' },
-            ]
+            // 模态框相关变量
+            dialogTitle: "", // 模态框标题
+            dialogVisible: false, // 控制用户信息对话框的显示
+            isView: false, // 是否为查看模式
+            isEdit: false, // 是否为修改模式
+
+            userInfo: { // 用户信息
+                no: "",
+                name: "",
+                gender: "",
+                birthday: "",
+                mobile: "",
+                email: "",
+                address: "",
+                career: "",
+                image: "",  // 头像
+            },
+
+            //添加用户的表单信息提交前的校验
+            rules: {
+                no: [
+                    { required: true, message: '账号不能为空', trigger: 'blur' },
+                    { pattern: /\d{5}$/, message: '账号必须是五位数', trigger: 'blur' },
+                    { validator: rulesNo, trigger: 'blur' }, //校验账号是否已经存在
+                ],
+                name: [
+                    { required: true, message: '姓名不能为空', trigger: 'blur' },
+                    { pattern: /^[\u4e00-\u9fa5]{2,5}$/, message: '姓名必须是2-5个汉字', trigger: 'blur' },
+                ],
+                gender: [
+                    { required: true, message: '性别不能为空', trigger: 'change' },
+                ],
+                birthday: [
+                    { type: 'date', required: true, message: '出生日期不能为空', trigger: 'change' },
+                ],
+                mobile: [
+                    { required: true, message: '手机号码不能为空', triggler: 'blur' },
+                    { pattern: /^[1][35789]\d{9}$/, message: '手机号码必须要符合规范', trigger: 'blur' },
+                ],
+                email: [
+                    { required: true, message: '邮箱地址不能为空', trigger: 'blur' },
+                    { pattern: /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/, message: '邮箱地址必须要符合规范', trigger: 'blur' },
+                ],
+                address: [
+                    { required: true, message: '家庭住址不能为空', trigger: 'blur' },
+                ],
+                career: [
+                    { required: true, message: '研究领域不能为空', trigger: 'blur' },
+                ]
+            }
         }
 
     },
+
 
     mounted() {
         // 打开页面自动加载所有用户信息
@@ -73,51 +109,51 @@ const app = new Vue ({
 
     methods: {
         // 获取所有用户信息
-        getUsers:function() {
+        getUsers: function () {
             // 记录this的地址，因为axios使用过程中this的地址会改变
             let true_this = this;
 
             // 使用Axios实现Ajas请求
             axios.get(true_this.baseURL + "users/")
-            .then(function(res) {
-                // 执行成功的回调函数
-                if(res.data.code === 1) {
-                    // # 后端返回
-                    // return JsonResponse({'code': 1, 'data': users})
-                    true_this.users = res.data.data;
+                .then(function (res) {
+                    // 执行成功的回调函数
+                    if (res.data.code === 1) {
+                        // # 后端返回
+                        // return JsonResponse({'code': 1, 'data': users})
+                        true_this.users = res.data.data;
 
-                    // 总数据数量
-                    true_this.total = res.data.data.length;
-                    true_this.getPageUsers();
+                        // 总数据数量
+                        true_this.total = res.data.data.length;
+                        true_this.getPageUsers();
 
-                    // 提示
-                    true_this.$message({
-                        message: '用户信息加载成功！',
-                        type: 'success'
-                    });
+                        // 提示
+                        true_this.$message({
+                            message: '用户信息加载成功！',
+                            type: 'success'
+                        });
 
-                }
-                else {
-                    // 失败的提示
-                    true_this.$message.error('res.data.msg');
-                }
-            })
-            .catch(function(err) {
-                // 执行失败的回调函数
-                console.log(err)
-            })
+                    }
+                    else {
+                        // 失败的提示
+                        true_this.$message.error('res.data.msg');
+                    }
+                })
+                .catch(function (err) {
+                    // 执行失败的回调函数
+                    console.log(err)
+                })
         },
 
         // 获取当前页的用户信息
         getPageUsers() {
-            this.pageUsers=[]; // 先清空数组
+            this.pageUsers = []; // 先清空数组
 
             // 获得当前页的数据
-            for(let i = (this.currentpage-1)*this.pagesize; i < this.total; i++) {
+            for (let i = (this.currentpage - 1) * this.pagesize; i < this.total; i++) {
                 this.pageUsers.push(this.users[i]);
 
                 // 如果当前页的数据已经达到pagesize，则退出循环
-                if(this.pageUsers.length == this.pagesize) {
+                if (this.pageUsers.length == this.pagesize) {
                     break;
                 }
             }
@@ -142,56 +178,56 @@ const app = new Vue ({
 
         // 用户信息查询
         queryUsers() {
-             //使用Ajax请求--POST-->传递InputStr
-             let true_this = this;
+            //使用Ajax请求--POST-->传递InputStr
+            let true_this = this;
 
-             //开始Ajax请求
+            //开始Ajax请求
             axios
-            .post(
-                true_this.baseURL + "users/query/",
-                {
-                    inputstr: true_this.inputStr, // 对于后端的inpustr
-                }
-            )
-            .then(function (res) {
-                if (res.data.code === 1) {
-                    //把数据给users
-                    true_this.users = res.data.data;
-                    //获取返回记录的总行数
-                    true_this.total = res.data.data.length;
+                .post(
+                    true_this.baseURL + "users/query/",
+                    {
+                        inputstr: true_this.inputStr, // 对于后端的inpustr
+                    }
+                )
+                .then(function (res) {
+                    if (res.data.code === 1) {
+                        //把数据给users
+                        true_this.users = res.data.data;
+                        //获取返回记录的总行数
+                        true_this.total = res.data.data.length;
 
-                    //查询数据为空
-                    if (true_this.users.length === 0) {
-                        
-                        true_this.$message({
-                            message: '查询数据为空！',
-                            type: 'warning'
-                        });
+                        //查询数据为空
+                        if (true_this.users.length === 0) {
 
-                        true_this.users = []; //清空数组
-                        //获取当前页的数据
-                        true_this.getPageUsers();
+                            true_this.$message({
+                                message: '查询数据为空！',
+                                type: 'warning'
+                            });
+
+                            true_this.users = []; //清空数组
+                            //获取当前页的数据
+                            true_this.getPageUsers();
+
+                        } else {
+                            //获取当前页的数据
+                            true_this.getPageUsers();
+                            //提示：
+                            true_this.$message({
+                                message: '查询数据加载成功！',
+                                type: 'success'
+                            });
+                        }
+
 
                     } else {
-                        //获取当前页的数据
-                        true_this.getPageUsers();
-                        //提示：
-                        true_this.$message({
-                            message: '查询数据加载成功！',
-                            type: 'success'
-                        });
+                        //失败的提示！
+                        true_this.$message.error(res.data.msg);
                     }
-
-                    
-                } else {
-                    //失败的提示！
-                    true_this.$message.error(res.data.msg);
-                }
-            })
-            .catch(function (err) {
-                console.log(err);
-                true_this.$message.error("获取后端查询结果出现异常！");
-            });
+                })
+                .catch(function (err) {
+                    console.log(err);
+                    true_this.$message.error("获取后端查询结果出现异常！");
+                });
         },
 
         //显示全部用户信息按钮
@@ -203,7 +239,7 @@ const app = new Vue ({
         //添加用户，打开表单（模态框）
         addUser() {
             this.dialogTitle = '添加用户信息';
-            this.dialogVisible = true;   
+            this.dialogVisible = true;
         },
 
         //查看用户信息，所有输入框禁用
@@ -242,11 +278,11 @@ const app = new Vue ({
 
             this.isEdit = true;
 
-            
+
 
             //深拷贝
             this.userInfo = JSON.parse(JSON.stringify(row));
         },
-        
+
     }
 })
