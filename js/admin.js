daa4c4dad12025b3fd68ea08e35af77c93c92a36
lichengyang <lichengyang1024@126.com>
2025-02-24 21:13:43 +0800

    添加用户信息表单信息项提交前的校验

--- js/admin.js
+++ js/admin.js
@@ -30,7 +30,40 @@ const app = new Vue ({
             address: "",
             career: "",
             image: "",  // 头像
+        },
+
+        //添加用户的表单信息提交前的校验
+        rules: {
+            no: [
+                { required: true, message: '账号不能为空', trigger: 'blur' },
+                { pattern: /\d{5}$/, message: '账号必须是五位数', trigger: 'blur' },
+            ],
+            name: [
+                { required: true, message: '姓名不能为空', trigger: 'blur' },
+                { pattern: /^[\u4e00-\u9fa5]{2,5}$/, message: '姓名必须是2-5个汉字', trigger: 'blur' },
+            ],
+            gender: [
+                { required: true, message: '性别不能为空', trigger: 'change' },
+            ],
+            birthday: [
+                { type:'date', required: true, message: '出生日期不能为空', trigger: 'change' },
+            ],
+            mobile: [
+                { required: true, message: '手机号码不能为空', triggler: 'blur' },
+                { pattern: /^[1][35789]\d{9}$/, message: '手机号码必须要符合规范', trigger: 'blur' },
+            ],
+            email: [
+                { required: true, message: '邮箱地址不能为空', trigger: 'blur' },
+                { pattern: /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/, message: '邮箱地址必须要符合规范', trigger: 'blur' },
+            ],
+            address: [
+                { required: true, message: '家庭住址不能为空', trigger: 'blur' },
+            ],
+            career: [
+                { required: true, message: '研究领域不能为空', trigger: 'blur' },
+            ]
         }
+
     },
 
     mounted() {
@@ -193,7 +226,7 @@ const app = new Vue ({
             this.userInfo.email = '';
             this.userInfo.mobile = '';
             this.userInfo.address = '';
-            this.userInfo.carer = '';
+            this.userInfo.career = '';
             this.userInfo.iamge = '';
 
             this.dialogVisible = false;
