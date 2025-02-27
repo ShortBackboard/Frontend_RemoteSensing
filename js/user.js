93c5677ad475a189e6a7f76c85dbba365ec894e5
lichengyang <lichengyang1024@126.com>
2025-02-27 19:31:46 +0800

    用户界面选择不同功能切换不同的Main界面

--- js/user.js
+++ js/user.js
@@ -1,6 +1,17 @@
-const app = new Vue ({
-    el:'#app',
-    data: {
+const app = new Vue({
+    el: '#app',
+    data() {
+        return {
             msg: '多平台遥感协同叶绿素浓度监测系统',
+
+            activeIndex: '1' // 当前激活的菜单项
         }
+    },
+
+    methods: {
+        handleSelect(index) {
+            this.activeIndex = index
+        }
+    },
+
 })
